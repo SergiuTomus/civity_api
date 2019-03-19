186b501e95ced5c761ccb69cbaa28114dfe76f74
sergiu.tomus <sergiu.tomus@reea.net>
2019-03-19 10:34:07 +0200

    update

--- app.js
+++ app.js
@@ -14,16 +14,16 @@ app.use(bodyParser.json());
 
 // Access Control middleware
 app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header(
-        "Access-Control-Allow-Headers",
-        "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-    );
-    if (req.method === "OPTIONS") {
-        res.header("Access-Control-Allow-Methods", "POST, PUT, PATCH, DELETE, GET");
-        return res.status(200).json({});
-    }
-    next();
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  if (req.method === "OPTIONS") {
+    res.header("Access-Control-Allow-Methods", "POST, PUT, PATCH, DELETE, GET");
+    return res.status(200).json({});
+  }
+  next();
 });
 
 // Passport middleware
@@ -36,19 +36,19 @@ app.use("/admin", adminRoutes);
 
 // Middleware for requests that does not matched with the defined routes
 app.use((req, res, next) => {
-    const error = new Error("Resursa nu a fost gasita");
-    error.status = 404;
-    next(error);
+  const error = new Error("Resursa nu a fost gasita");
+  error.status = 404;
+  next(error);
 });
 
 // Middleware for all errors in the app
 app.use((error, req, res, next) => {
-    res.status(error.status || 500);
-    res.json({
-        error: {
-            message: error.message
-        }
-    });
+  res.status(error.status || 500);
+  res.json({
+    error: {
+      message: error.message
+    }
+  });
 });
 
 
--- validations/login.js
+++ validations/login.js
@@ -2,23 +2,23 @@ const Validator = require('validator');
 const isEmpty = require('./isEmpty');
 
 const validateLogin = (data) => {
-    let errors = {};
+  let errors = {};
 
-    data.email = !isEmpty(data.email) ? data.email : '';
-    data.password = !isEmpty(data.password) ? data.password : '';
+  data.email = !isEmpty(data.email) ? data.email : '';
+  data.password = !isEmpty(data.password) ? data.password : '';
 
-    if (Validator.isEmpty(data.email)) {
-        errors.email = "Campul email trebuie completat";
-    }
-    if (Validator.isEmpty(data.password)) {
-        errors.password = 'Campul parola trebuie completat';
-    }
+  if (Validator.isEmpty(data.email)) {
+    errors.email = "Campul email trebuie completat";
+  }
+  if (Validator.isEmpty(data.password)) {
+    errors.password = 'Campul parola trebuie completat';
+  }
 
 
-    return {
-        errors,
-        isValid: isEmpty(errors)
-    };
+  return {
+    errors,
+    isValid: isEmpty(errors)
+  };
 };
 
 module.exports = validateLogin;
